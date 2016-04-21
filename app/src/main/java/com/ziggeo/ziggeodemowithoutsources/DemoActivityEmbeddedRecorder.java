242957653be84cc5bdf4779e17f9f123e22ff1ca
Alexandr Bedulin <alexandr.bedulin@gmail.com>
2016-04-21 13:07:03 +0300

    event subscribing demo code added.

--- app/src/main/java/com/ziggeo/ziggeodemowithoutsources/DemoActivityEmbeddedRecorder.java
+++ app/src/main/java/com/ziggeo/ziggeodemowithoutsources/DemoActivityEmbeddedRecorder.java
@@ -2,8 +2,14 @@ package com.ziggeo.ziggeodemowithoutsources;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.common.eventbus.Subscribe;
 import com.ziggeo.androidsdk.Ziggeo;
+import com.ziggeo.androidsdk.eventbus.BusProvider;
+import com.ziggeo.androidsdk.eventbus.events.CreateVideoErrorEvent;
+import com.ziggeo.androidsdk.eventbus.events.VideoSentEvent;
+import com.ziggeo.androidsdk.recording.CameraHelper;
 
 public class DemoActivityEmbeddedRecorder extends Activity {
 
@@ -14,11 +20,23 @@ public class DemoActivityEmbeddedRecorder extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        BusProvider.getInstance().register(this);
+
         setContentView(R.layout.activity_embedded_recorder);
 
         Ziggeo ziggeo = new Ziggeo(APP_TOKEN);
         long maxDuration = 20000L;
-        ziggeo.attachRecorder(getFragmentManager(), R.id.fl_content, maxDuration);
+        ziggeo.attachRecorder(getFragmentManager(), R.id.fl_content, maxDuration, CameraHelper.Quality.LOW);
     }
 
+    @Subscribe
+    public void onVideoSent(VideoSentEvent event) {
+        Log.e(TAG, "Video successfully sent. Video token is: " + event.getVideoToken());
+    }
+
+    @Subscribe
+    public void onCreateVideoError(CreateVideoErrorEvent event) {
+        Log.e(TAG, "Error while sending the video.");
+    }
 }
--- app/src/main/java/com/ziggeo/ziggeodemowithoutsources/DemoActivityFullscreenRecorder.java
+++ app/src/main/java/com/ziggeo/ziggeodemowithoutsources/DemoActivityFullscreenRecorder.java
@@ -2,8 +2,13 @@ package com.ziggeo.ziggeodemowithoutsources;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.common.eventbus.Subscribe;
 import com.ziggeo.androidsdk.Ziggeo;
+import com.ziggeo.androidsdk.eventbus.BusProvider;
+import com.ziggeo.androidsdk.eventbus.events.CreateVideoErrorEvent;
+import com.ziggeo.androidsdk.eventbus.events.VideoSentEvent;
 
 public class DemoActivityFullscreenRecorder extends Activity {
 
@@ -15,9 +20,21 @@ public class DemoActivityFullscreenRecorder extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        BusProvider.getInstance().register(this);
+
         Ziggeo ziggeo = new Ziggeo(APP_TOKEN);
         long maxDuration = 20000L;
         ziggeo.createVideo(this, maxDuration);
+    }
+
+    @Subscribe
+    public void onVideoSent(VideoSentEvent event) {
+        Log.e(TAG, "Video successfully sent. Video token is: " + event.getVideoToken());
+    }
+
+    @Subscribe
+    public void onCreateVideoError(CreateVideoErrorEvent event) {
+        Log.e(TAG, "Error while sending the video.");
     }
 
 }
