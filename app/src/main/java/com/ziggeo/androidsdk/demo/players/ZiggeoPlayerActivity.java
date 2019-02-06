84c8705e3d577aa4cdb0c8b04a46fcd17522c3cc
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-02-06 10:50:14 +0300

    Player configuration changed.

--- app/src/main/java/com/ziggeo/androidsdk/demo/players/ZiggeoPlayerActivity.java
+++ app/src/main/java/com/ziggeo/androidsdk/demo/players/ZiggeoPlayerActivity.java
@@ -1,14 +1,13 @@
 package com.ziggeo.androidsdk.demo.players;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.util.SparseArray;
 
 import com.ziggeo.androidsdk.Ziggeo;
 import com.ziggeo.androidsdk.callbacks.PlayerCallback;
 import com.ziggeo.androidsdk.demo.BaseActivity;
+import com.ziggeo.androidsdk.player.PlayerConfig;
 import com.ziggeo.androidsdk.ui.theming.PlayerStyle;
-import com.ziggeo.androidsdk.ui.theming.ZiggeoTheme;
 
 import timber.log.Timber;
 
@@ -21,62 +20,65 @@ public class ZiggeoPlayerActivity extends BaseActivity {
     protected void init() {
         super.init();
         Ziggeo ziggeo = new Ziggeo(APP_TOKEN, this);
-        ziggeo.setPlayerCallbacks(new PlayerCallback() {
-            @Override
-            public void loaded() {
-                super.loaded();
-                Timber.d("Player. Loaded");
-            }
-
-            @Override
-            public void error(@NonNull Throwable throwable) {
-                super.error(throwable);
-                Timber.d(throwable, "Player. Error");
-            }
-
-            @Override
-            public void playing() {
-                super.playing();
-                Timber.d("Player. Playing");
-            }
-
-            @Override
-            public void paused() {
-                super.paused();
-                Timber.d("Player. Paused");
-            }
-
-            @Override
-            public void ended() {
-                super.ended();
-                Timber.d("Player. Ended");
-            }
-
-            @Override
-            public void seek(long millis) {
-                super.seek(millis);
-                Timber.d("Player. Seek");
-            }
-
-            @Override
-            public void readyToPlay() {
-                super.readyToPlay();
-                Timber.d("Player. Ready to play");
-            }
-        });
+        configurePlayer(ziggeo);
         // video tokens or keys here
         String videoToPlay1 = "";
         ziggeo.startPlayer(videoToPlay1);
     }
 
     /**
-     * Also see styles.xml for styling through XML
+     * Also see styles.xml for configurePlayer through XML
      */
-    private void styling(@NonNull Ziggeo ziggeo) {
-        ZiggeoTheme theme = new ZiggeoTheme();
-        PlayerStyle playerStyle = new PlayerStyle.Builder()
-                .controllerStyle(PlayerStyle.DEFAULT)
+    private void configurePlayer(@NonNull Ziggeo ziggeo) {
+        PlayerConfig playerConfig = new PlayerConfig.Builder()
+                .style(new PlayerStyle.Builder()
+                        .controllerStyle(PlayerStyle.DEFAULT)
+                        .build())
+                .callback(new PlayerCallback() {
+                    @Override
+                    public void loaded() {
+                        super.loaded();
+                        Timber.d("Player. Loaded");
+                    }
+
+                    @Override
+                    public void error(@NonNull Throwable throwable) {
+                        super.error(throwable);
+                        Timber.d(throwable, "Player. Error");
+                    }
+
+                    @Override
+                    public void playing() {
+                        super.playing();
+                        Timber.d("Player. Playing");
+                    }
+
+                    @Override
+                    public void paused() {
+                        super.paused();
+                        Timber.d("Player. Paused");
+                    }
+
+                    @Override
+                    public void ended() {
+                        super.ended();
+                        Timber.d("Player. Ended");
+                    }
+
+                    @Override
+                    public void seek(long millis) {
+                        super.seek(millis);
+                        Timber.d("Player. Seek");
+                    }
+
+                    @Override
+                    public void readyToPlay() {
+                        super.readyToPlay();
+                        Timber.d("Player. Ready to play");
+                    }
+                })
                 .build();
-        theme.setPlayerStyle(playerStyle);
+
+        ziggeo.configurePlayer(playerConfig);
     }
 }
