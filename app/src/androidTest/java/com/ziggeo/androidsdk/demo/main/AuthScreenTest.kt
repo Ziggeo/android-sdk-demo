53b2a130094a3677ba5ece019be8c8333bb9983d
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-10-09 07:21:10 +0300

    Added back press test for AuthScreen

--- app/src/androidTest/java/com/ziggeo/androidsdk/demo/main/AuthScreenTest.kt
+++ app/src/androidTest/java/com/ziggeo/androidsdk/demo/main/AuthScreenTest.kt
@@ -1,13 +1,14 @@
 package com.ziggeo.androidsdk.demo.main
 
+import androidx.test.espresso.NoActivityResumedException
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import com.agoda.kakao.screen.Screen.Companion.onScreen
-import com.ziggeo.androidsdk.demo.R
 import com.ziggeo.androidsdk.demo.ui.AppActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class AuthScreenTest {
@@ -21,17 +22,25 @@ class AuthScreenTest {
         onScreen<AuthScreen> {
             // is logo visible and has correct image
             ivLogo.isDisplayed()
-            ivLogo.hasDrawable(R.drawable.ic_ziggeo_logo)
+            ivLogo.hasDrawable(com.ziggeo.androidsdk.demo.R.drawable.ic_ziggeo_logo)
 
             // is message visible and has correct text
             tvMessage.isDisplayed()
-            tvMessage.hasText(R.string.auth_message)
+            tvMessage.hasText(com.ziggeo.androidsdk.demo.R.string.auth_message)
 
             // is button visible, has correct text and enabled
             btnScanQr.isDisplayed()
             btnScanQr.isEnabled()
             btnScanQr.isClickable()
-            btnScanQr.hasText(R.string.btn_scan_qr_text)
+            btnScanQr.hasText(com.ziggeo.androidsdk.demo.R.string.btn_scan_qr_text)
         }
     }
+
+    @Test(expected = NoActivityResumedException::class)
+    fun testBackPress() {
+        onScreen<AuthScreen> {
+            pressBack()
+        }
+    }
+
 }
