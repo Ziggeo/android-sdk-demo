c9d4784a234a17a4facf836224eda97f695668c1
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-10-24 16:00:52 +0300

    About screen implemented

--- app/src/main/java/com/ziggeo/androidsdk/demo/ui/about/AboutFragment.kt
+++ app/src/main/java/com/ziggeo/androidsdk/demo/ui/about/AboutFragment.kt
@@ -1,11 +1,19 @@
 package com.ziggeo.androidsdk.demo.ui.about
 
+import android.os.Build
+import android.os.Bundle
+import android.text.Html
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.view.View
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.ziggeo.androidsdk.demo.R
 import com.ziggeo.androidsdk.demo.presentation.about.AboutPresenter
 import com.ziggeo.androidsdk.demo.presentation.about.AboutView
 import com.ziggeo.androidsdk.demo.ui.global.BaseToolbarFragment
+import kotlinx.android.synthetic.main.fragment_about.*
 
 
 /**
@@ -25,4 +33,21 @@ class AboutFragment : BaseToolbarFragment<AboutView, AboutPresenter>(), AboutVie
 
     override fun getTitleRes() = R.string.title_about
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tv_text.text = fromHtml(getString(R.string.about_text).replace("\n","<br>"))
+        tv_text.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun fromHtml(html: String?): Spanned {
+//        return when {
+//            html == null -> SpannableString("")
+//            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> Html.fromHtml(
+//                html,
+//                Html.FROM_HTML_MODE_LEGACY
+//            )
+//            else ->
+           return     Html.fromHtml(html)
+//        }
+    }
 }
